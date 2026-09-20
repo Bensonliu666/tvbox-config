836d3b309d68c24ec24d39d18e6389720aaf9fef
Bensonliu666 <53586027+Bensonliu666@users.noreply.github.com>
2026-09-20 11:59:30 +0800

    fix rrdyw category parse

--- js/rrdyw.js
+++ js/rrdyw.js
@@ -27,7 +27,7 @@ var rule = {
     lazy: '',
     limit: 6,
     推荐: '',
-    一级: 'li.pure-g;img&&alt;img&&data-original;a&&href',
+    一级: 'li.shadow;h2 a&&title;img&&data-original;;a.movie-thumbnails&&href',
     二级: {
         title: "h1&&Text",
         img: "img&&src",
